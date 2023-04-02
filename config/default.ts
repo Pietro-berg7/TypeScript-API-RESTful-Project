a0485c4cb3ca2d8d6c11c386eced829ef794abee
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-02 06:20:19 +0044

    Integrando o winston

--- config/default.ts
+++ config/default.ts
@@ -4,4 +4,5 @@ const dbPassword = process.env.DB_PASS;
 export default {
   port: 3000,
   dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.zs8yvyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  env: "development",
 };
